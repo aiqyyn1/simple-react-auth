eb91681aa0fe924d36290d19be12ebe4b1dc5d8a
Aikyn <ibrayevaiqyn@gmail.com>
2023-10-02 15:46:14 +0600

    fix:some bugs

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,12 +1,12 @@
 import SignUp from '../components/auth/SignUp';
 import RestorePassword from '../components/auth/RestorePassword';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes, BrowserRouter } from 'react-router-dom';
 import SignIn from '../components/auth/SignIn';
 
 function App() {
   return (
     <>
-      <HashRouter basename='/simple-react-auth'>
+      <HashRouter>
         <Routes>
           <Route path='/' index element={<SignIn></SignIn>}></Route>
           <Route path='/signup' element={<SignUp></SignUp>}></Route>
